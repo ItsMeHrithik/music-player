3fa927ea5e77b1221b1c55f056585cede2b0ff3a
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-10 20:09:54 +0530

    template of the player is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./styles/app.scss";
+import Song from "./components/Songs";
+import Player from "./components/Player";
 
 const App = () => {
   return (
-    <div>
-      <h1>Music Player</h1>
+    <div className="App">
+        <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 };
